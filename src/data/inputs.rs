27fe84a8006de146e78ef3695565ae3ec3fa34f7
soul <sulayman.aaliy@gmail.com>
2023-09-06 01:25:00 +0200

    add a few more tests

--- src/data/inputs.rs
+++ src/data/inputs.rs
@@ -12,7 +12,7 @@ pub fn compute_from(inputs: &mut Vec<IndicatorInput>, key: &ComputeKey) -> Resul
 #[cfg(test)]
 mod tests {
     use crate::{ComputeKey, FY, IndicatorInput, date::DateKey};
-    use super::compute_from;
+    use super::*;
 
     #[test]
     //#[should_panic(expected = "Indicator inputs list is empty")]
@@ -20,6 +20,17 @@ mod tests {
         let mut v: Vec<IndicatorInput> = vec![];
         let key = ComputeKey { class: Some(&FY), date: DateKey::build(1, 1) };
         compute_from(&mut v, &key);
-        assert!(v.len() > 0);
+        assert!(v.iter().filter(|i| i.key.class == Some(&FY)).count() > 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Indicator inputs list is empty")]
+    fn compute_from_empty() {
+        let mut v: Vec<IndicatorInput> = vec![];
+        let key = ComputeKey { class: Some(&FY), date: DateKey::build(1, 1) };
+        match compute_from(&mut v, &key) {
+            Err(e) => panic!("{e}"),
+            _ => {}
+        };
     }
 }
